715c1fd0269b2546aeb2c8b8d6d306527419d63f
Noam Anisfeld <NoamAnisfeld@gmail.com>
2023-08-24 00:41:51 +0300

    configuring the mockup server to work in the browser

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,10 @@ import RTLProvider from './RTLProvider'
 
 const store = setupStore();
 
+if (process.env.NODE_ENV === 'development') {
+  import('./mocks/browser').then(({ worker }) => worker.start());
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ReduxProvider store={store}>
