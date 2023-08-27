c0dccb57980a7e40011b18a137557185cc62426d
Noam Anisfeld <NoamAnisfeld@gmail.com>
2023-08-27 12:47:09 +0300

    add data validation with zod

--- src/requests/getCourses.ts
+++ src/requests/getCourses.ts
@@ -1,11 +1,38 @@
+import { ZodError, z } from 'zod';
+
+const course = z.object({
+    course_id: z.string(),
+    course_name: z.string(),
+});
+type Course = z.infer<typeof course>;
+
+const courses = z.array(course);
+export type Courses = z.infer<typeof courses>;
+
 if (process.env.NODE_ENV === 'development') {
     import('../mocks/browser').then(({ worker }) => worker.start());
 }
 
 export async function getCourses() {
-    const
-        data = await fetch('/courses'),
-        jsonData = await data.json();
-    
-    return jsonData;
+    await new Promise(res => setTimeout(res, 1000));
+
+    try {
+        const
+            fetchData = await fetch('/courses'),
+            jsonData: Courses = await fetchData.json(),
+            validatedData = courses.parse(jsonData);
+
+        return validatedData;
+
+    } catch (e) {
+        if (e instanceof SyntaxError) {
+            console.log('invalid JSON');
+            return [];
+        } else if (e instanceof ZodError) {
+            console.log('invalid data');
+            return []
+        } else {
+            throw e;
+        }
+    }
 }
--- src/routes/MyCourses.tsx
+++ src/routes/MyCourses.tsx
@@ -1,19 +1,21 @@
 import { useState } from "react"
-import { getCourses } from "../requests/getCourses"
+import { getCourses, Courses } from "../requests/getCourses"
 import { Stack } from "@mui/material"
 import CourseCard from "../components/CourseCard"
 
 export default function MyCourses() {
-    const [coursesData, setCoursesData] = useState([]);
+    const [coursesData, setCoursesData] = useState<Courses>([]);
 
     if (!coursesData.length)
         getCourses().then(setCoursesData);
 
     return <Stack spacing={5} minWidth="80%">
-        {coursesData.map(course => <CourseCard
+        {coursesData.length ?
+        coursesData.map(course => <CourseCard
             key={course.course_id}
             id={course.course_id}
             name={course.course_name}
-        />)}
+        />)
+        : "טוען..."}
     </Stack>
 }
