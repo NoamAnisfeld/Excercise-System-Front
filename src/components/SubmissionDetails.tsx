2198ee85d704b0ab077bd1cccb8a720018d94a80
Noam Anisfeld <NoamAnisfeld@gmail.com>
2023-09-14 16:21:30 +0300

    clear a DOM structure warning

--- src/components/SubmissionDetails.tsx
+++ src/components/SubmissionDetails.tsx
@@ -1,4 +1,4 @@
-import { Table, TableRow, TableCell, Button } from '@mui/material'
+import { Table, TableBody, TableRow, TableCell, Button } from '@mui/material'
 import type { SubmissionInfo } from "../requests/schemes"
 import { formatDateTime, formatDate } from '../utils';
 
@@ -11,39 +11,41 @@ export default function SubmissionDetails({
 }: SubmissionInfo) {
     return (
         <Table sx={{ width: "auto", margin: "auto" }}>
-            <TableRow>
-                <TableCell component="th">הוגש בתאריך</TableCell>
-                <TableCell>{formatDateTime(created_at)}</TableCell>
-            </TableRow>
+            <TableBody>
+                <TableRow>
+                    <TableCell component="th">הוגש בתאריך</TableCell>
+                    <TableCell>{formatDateTime(created_at)}</TableCell>
+                </TableRow>
 
-            {updated_at !== created_at ?
-            <TableRow>
-                <TableCell component="th">עודכן בתאריך</TableCell>
-                <TableCell>{formatDateTime(updated_at)}</TableCell>
-            </TableRow>
-            : undefined}
+                {updated_at !== created_at ?
+                    <TableRow>
+                        <TableCell component="th">עודכן בתאריך</TableCell>
+                        <TableCell>{formatDateTime(updated_at)}</TableCell>
+                    </TableRow>
+                    : undefined}
 
-            <TableRow>
-                <TableCell component="th">קובץ</TableCell>
-                <TableCell><a href={file} download>{file}</a></TableCell>
-            </TableRow>
+                <TableRow>
+                    <TableCell component="th">קובץ</TableCell>
+                    <TableCell><a href={file} download>{file}</a></TableCell>
+                </TableRow>
 
-            <TableRow>
-                <TableCell component="th">משוב</TableCell>
-                <TableCell>{comment}</TableCell>
-            </TableRow>
+                <TableRow>
+                    <TableCell component="th">משוב</TableCell>
+                    <TableCell>{comment}</TableCell>
+                </TableRow>
 
-            <TableRow>
-                <TableCell component="th">תאריך אחרון לעדכון</TableCell>
-                <TableCell>
-                    {new Date(sub_date) <= new Date() ?
-                    <>
-                        {formatDate(sub_date)}
-                        <Button variant="contained" size="small" sx={{ mx: 2 }}>עדכון</Button>
-                    </>
-                    : 'עבר'}
-                </TableCell>
-            </TableRow>
+                <TableRow>
+                    <TableCell component="th">תאריך אחרון לעדכון</TableCell>
+                    <TableCell>
+                        {new Date(sub_date) <= new Date() ?
+                            <>
+                                {formatDate(sub_date)}
+                                <Button variant="contained" size="small" sx={{ mx: 2 }}>עדכון</Button>
+                            </>
+                            : 'עבר'}
+                    </TableCell>
+                </TableRow>
+            </TableBody>
         </Table>
     );
 }
