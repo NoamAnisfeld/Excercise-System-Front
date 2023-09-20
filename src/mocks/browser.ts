9c5cb9c1e1183bd5b3e8ad4714a93a105055a8d1
Noam Anisfeld <NoamAnisfeld@gmail.com>
2023-09-20 15:09:27 +0300

    Move mock Service Worker logic to the appropriate place

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,4 +1,25 @@
 import { setupWorker } from 'msw'
 import { handlers } from './handlers'
 
-export const worker = setupWorker(...handlers)
+const worker = setupWorker(...handlers);
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string || location.origin;
+worker.start({
+    onUnhandledRequest(req) {
+        if (req.url.origin === location.origin) {
+            if (req.url.pathname.startsWith('/api/')) {
+                console.warn('Unhandled same-origin API request: ' + req.url.href);
+            } else {
+                console.log('Non-API same-origin request: ' + req.url.href);
+            }
+        } else if (req.url.origin === API_BASE_URL) {
+            if (req.url.pathname.startsWith('/api/')) {
+                console.log('API request passed to the API server: ' + req.url.href);
+            } else {
+                console.warn('Non-API request passed to the API server: ' + req.url.href);
+            }
+        } else {
+            console.log('External request: ' + req.url.href)
+        }
+    }
+});
--- src/requests/fetchers.ts
+++ src/requests/fetchers.ts
@@ -5,30 +5,10 @@ import {
     submissionInfoScheme, submissionsScheme,
 } from './schemes'
 
+if (process.env.NODE_ENV === 'development')
+    import('../mocks/browser');
+
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string || location.origin;
-
-if (process.env.NODE_ENV === 'development') {
-    await import('../mocks/browser').then(({ worker }) => worker.start({
-        onUnhandledRequest(req) {
-            if (req.url.origin === location.origin) {
-                if (req.url.pathname.startsWith('/api/')) {
-                    console.warn('Unhandled same-origin API request: ' + req.url.href);
-                } else {
-                    console.log('Non-API same-origin request: ' + req.url.href);
-                }
-            } else if (req.url.origin === API_BASE_URL) {
-                if (req.url.pathname.startsWith('/api/')) {
-                    console.log('API request passed to the API server: ' + req.url.href);
-                } else {
-                    console.warn('Non-API request passed to the API server: ' + req.url.href);
-                }
-            } else {
-                console.log('External request: ' + req.url.href)
-            }
-        }
-    })
-)}
-
 
 async function fetchApiData<T>(
     path: string,
