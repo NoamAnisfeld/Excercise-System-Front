7fb3eb0159b549b75b2efbb6d957547f10fc4c0b
Noam Anisfeld <NoamAnisfeld@gmail.com>
2023-12-18 00:35:14 +0200

    adding test snapshots to some components

--- src/components/main/Login.test.tsx
+++ src/components/main/Login.test.tsx
@@ -1,9 +1,9 @@
 import { test, expect } from 'vitest'
 import { wrapWithStore, wrapWithRouter } from '../../test-utils'
 import { render } from '@testing-library/react'
-import MainLayout from './MainLayout'
+import Login from './Login'
 
-test('MainLayout', () => {
-    const result = render(wrapWithStore(wrapWithRouter(<MainLayout />)))
+test('Login', () => {
+    const result = render(wrapWithStore(wrapWithRouter(<Login />)))
     expect(result).toMatchSnapshot();
 })
